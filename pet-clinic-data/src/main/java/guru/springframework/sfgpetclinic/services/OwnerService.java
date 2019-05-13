d3b30ee09313d6a855af9e3cfe9bbde87b71a4ed
Bojan Sutulovic <bsutulovic@novomaticls.com>
2019-05-13 12:04:56 +0200

    Added spring data jpa service for owners. Closes #40

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -3,6 +3,6 @@ package guru.springframework.sfgpetclinic.services;
 import guru.springframework.sfgpetclinic.model.Owner;
 
 public interface OwnerService extends CrudService<Owner, Long> {
-    Owner findByLastNmae(String lastName);
+    Owner findByLastName(String lastName);
 
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -66,7 +66,7 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
   }
 
   @Override
-  public Owner findByLastNmae(String lastName) {
+  public Owner findByLastName(String lastName) {
     return null;
   }
 }
